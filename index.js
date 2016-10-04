77f5243b73f6c9c2fb45995061d92c5b3bde0f1a
davidkc0 <dvid1994@gmail.com>
2016-10-03 20:59:06 -0700

    added page access token and ecco

--- index.js
+++ index.js
@@ -27,4 +27,47 @@ app.get('/webhook/', function (req, res) {
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
+}) 
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
 })
+
+var token = "<EAAWeGDAD7cQBAHHc3AHIgqGrnmn5UasQcZAZCy1CujVewI7KzFZCpO2chx2GtWXjuDA7QVclcQ0sEXNmgVh3XQZAuMFWh0KSoWX37ZAwi0xjkWhfRngXwiZBVxRrZAg83Ay3LwIGrOW2yuWt8YSadmG3goyZAmvKb6urE3pLkpjgUAZDZD>"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+
+
+
+
+
