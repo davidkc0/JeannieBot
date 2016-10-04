0ee2bc51bb25f38c2aa38a6628a2807701aa297e
davidkc0 <dvid1994@gmail.com>
2016-10-03 20:31:17 -0700

    fixed facebook issue

--- index.js
+++ index.js
@@ -18,11 +18,19 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === '19283746534_hook_you') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === token) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
 
 // Spin up the server
 app.listen(app.get('port'), function() {
